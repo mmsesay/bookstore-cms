63dce41e67e5737c52f64588974ad8a2900d6291
mmsesay <maej_01@hotmail.com>
2021-11-02 11:43:22 +0000

    Add: useSelector hook to get data from the state Add: onChange event to the inputs

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,5 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
-const READ_BOOKS = 'bookStore/books/READ_BOOKS';
 
 // init state
 const initialState = [];
@@ -16,10 +15,6 @@ export const removeBook = (payload) => ({
   payload,
 });
 
-export const readBooks = () => ({
-  type: READ_BOOKS,
-});
-
 // reducer
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -31,9 +26,6 @@ const booksReducer = (state = initialState, action) => {
 
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload.id);
-
-    case READ_BOOKS:
-      return state;
 
     default:
       return state;
