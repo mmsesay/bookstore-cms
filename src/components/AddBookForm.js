5a654601f59f07bf5fb279f688e1c2f6f4b2a1c8
mmsesay <maej_01@hotmail.com>
2021-11-05 16:50:55 +0000

    Add form styling and message box

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -37,27 +37,34 @@ export const AddBookForm = () => {
   }, [message]);
 
   return (
-    <div>
-      <h2>ADD NEW BOOK</h2>
-      <p>{message}</p>
-
-      <form method="POST" onSubmit={handleFormSubmit}>
-        <input 
-          type="text"
-          placeholder="Author Name" 
-          value={author}
-          name="author"
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <br/>
+    <div className="border mx-20 p-10 mt-10">
+      <h2 className="font-montserrat font-bold text-3xl text-gray-500">ADD NEW BOOK</h2>
+      {
+        message 
+        && <div className="bg-red-100 w-2/3 h-16 flex items-center justify-center rounded my-3">
+          <p className="text-center text-red-600 text-2xl font-montserrat font-bold">{message}</p>
+        </div>
+      }
+      <form method="POST" onSubmit={handleFormSubmit} className="mt-5 flex flex-col">
         <input 
           type="text"
           placeholder="Book title" 
           value={title}
           name="title"
           onChange={(e) => setTitle(e.target.value)}
+          className="my-3 w-2/3 p-4 font-montserrat text-3xl text-gray-500 border border-blue-400 focus:outline-none"
         />
-        <button type="submit">Submit</button>
+        <input 
+          type="text"
+          placeholder="Author Name" 
+          value={author}
+          name="author"
+          onChange={(e) => setAuthor(e.target.value)}
+          className="my-3 w-2/3 p-4 font-montserrat text-3xl text-gray-500 border border-blue-400 focus:outline-none"
+        />
+        <button type="submit" className="font-montserrat bg-blue-500 text-white p-2 rounded mt-4 focus:outline-none w-40 p-3">
+          SUBMIT
+        </button>
       </form>
     </div>
   );
