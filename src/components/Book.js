63f8aa33c926a30b5546c7f7dc5a4712c29518ed
mmsesay <maej_01@hotmail.com>
2021-11-05 11:55:26 +0000

    Add: progress bar to the book card

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { removeBook } from '../redux/books/books';
 import { deleteBookFromApi } from '../redux/api';
 
@@ -16,20 +18,34 @@ export const Book = (prop) => {
 
   return (
     <>
-      <li key={item_id} className="Lesson-Panel">
-        <p className="Book-Title">{title}</p>
-        <p className="Author-Name text-blue-400">{category}</p>
-        <span className="flex mt-8">
-          <p className="text-blue-400 font-crete">Comments</p>
-          <span className="border-r mx-2"></span>
-          <p
-            className="text-blue-400 font-crete"
-            type="button"
-            onClick={(e) => handleRemove(e, item_id)}>
-            Remove
-          </p>
-          <span className="border-r mx-2"></span>
-          <p className="text-blue-400 font-crete">Edit</p>
+      <li key={item_id} className="Lesson-Panel flex space-x-10">
+        <span>
+          <p className="Book-Title">{title}</p>
+          <p className="Author-Name text-blue-400">{category}</p>
+          <span className="flex mt-8">
+            <p className="text-blue-400 font-crete">Comments</p>
+            <span className="border-r mx-2"></span>
+            <p
+              className="text-blue-400 font-crete"
+              type="button"
+              onClick={(e) => handleRemove(e, item_id)}>
+              Remove
+            </p>
+            <span className="border-r mx-2"></span>
+            <p className="text-blue-400 font-crete">Edit</p>
+          </span>
+        </span>
+        <span className="flex space-x-3">
+          <div className="w-24 h-24">
+            <CircularProgressbar value={66} />
+          </div>
+          <div className="">
+            <p>64%</p>
+            <p>Completed</p>
+          </div>
+        </span>
+        <span>
+          Current
         </span>
       </li>
       <br />
